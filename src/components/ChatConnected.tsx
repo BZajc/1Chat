a3539f69581b81ee3fc1924052ee9854b53dae78
BZajc <bbb.zaj@gmail.com>
2024-04-16 22:52:29 +0200

    Small UI fixes, expanded chatSlice and added half of the chat functionality (sending messages etc)

--- src/components/ChatConnected.tsx
+++ src/components/ChatConnected.tsx
@@ -9,18 +9,29 @@ import {
   FaUserPlus,
   FaUserSlash,
   FaRegPaperPlane,
+  FaArrowRightToBracket,
+  FaRegSquarePlus,
+  FaFaceLaughBeam,
 } from "react-icons/fa6";
 import {
   setUserImage,
   selectUserImage,
   setUserName,
   selectUserName,
+  addMessage,
+  selectMessages,
+  setConnected,
+  removeMessages
 } from "../store/slices/chatSlice";
+
 function ChatConnected() {
-  const [buttons, showButtons] = useState(false);
+  const [userButtons, showUserButtons] = useState(false);
+  const [inputButtons, showInputButtons] = useState(false);
+  const [inputValue, setInputValue] = useState("");
   const dispatch = useDispatch();
   const userImage = useSelector(selectUserImage);
   const userName = useSelector(selectUserName);
+  const messages = useSelector(selectMessages);
 
   useEffect(() => {
     if (!userImage) {
@@ -31,6 +42,42 @@ function ChatConnected() {
     }
   }, [dispatch, userImage, userName]);
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setInputValue("");
+    dispatch(addMessage({ message: inputValue, type: "right" }));
+    setTimeout(() => {
+      dispatch(
+        addMessage({
+          message: faker.lorem.words(Math.floor(Math.random() * 13) + 3), //Generate random words between 3 and 15
+          type: "left",
+        })
+      );
+    }, 1000);
+  };
+
+  const displayMessage = () => {
+    return messages.map((message, index) => (
+      <div key={index} className={`chat__connected-${message.type}-message`}>
+        {message.message}
+      </div>
+    ));
+  };
+
+  const onMoreButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    showInputButtons(!inputButtons);
+  };
+
+  const onLeaveChat = () => {
+    dispatch(setConnected(false));
+    dispatch(removeMessages());
+  }
+
   return (
     <div className="chat__connected-box">
       <div className="chat__connected-user-info">
@@ -40,52 +87,84 @@ function ChatConnected() {
         </div>
         <button
           className="chat__user-button"
-          onClick={() => showButtons(!buttons)}
+          onClick={() => showUserButtons(!userButtons)}
         >
           <PiUserFocusLight />
         </button>
       </div>
       <ul
         className={`chat__connected-buttons-box ${
-          buttons ? "chat__connected-buttons-box-active" : ""
+          userButtons ? "chat__connected-buttons-box-active" : ""
         }`}
       >
         <li className="chat__connected-item">
           <button className="chat__connected-button">
-            <FaUser className="chat__connected-button-icon chat__connected-button-icon--1" />
+            <FaUser className="chat__connected-button-icon " />
           </button>
         </li>
         <li className="chat__connected-item">
           <button className="chat__connected-button">
-            <FaThumbsUp className="chat__connected-button-icon chat__connected-button-icon--2" />
+            <FaThumbsUp className="chat__connected-button-icon " />
           </button>
         </li>
         <li className="chat__connected-item">
           <button className="chat__connected-button">
-            <FaThumbsDown className="chat__connected-button-icon chat__connected-button-icon--3" />
+            <FaThumbsDown className="chat__connected-button-icon " />
           </button>
         </li>
         <li className="chat__connected-item">
           <button className="chat__connected-button">
-            <FaUserPlus className="chat__connected-button-icon chat__connected-button-icon--4" />
+            <FaUserPlus className="chat__connected-button-icon " />
           </button>
         </li>
         <li className="chat__connected-item">
           <button className="chat__connected-button">
-            <FaUserSlash className="chat__connected-button-icon chat__connected-button-icon--5" />
+            <FaUserSlash className="chat__connected-button-icon " />
           </button>
         </li>
       </ul>
 
-      <div className="chat__input-box">
+      <h3 className="chat__successfully-connected">
+        You have been connected to{" "}
+        <span className="chat__yellow-text">{userName}</span>. Say hello
+      </h3>
+      <div className="chat__messages-box">{displayMessage()}</div>
+      <div className="chat__messages-fix"></div>
+
+      <form className="chat__input-box" onSubmit={onSubmit}>
         <input
           type="text"
           className="chat__input"
           placeholder="Type a message..."
+          value={inputValue}
+          onChange={onChange}
         />
         <button className="chat__send-button">
-            <FaRegPaperPlane className="chat__send-icon"/>
+          <FaRegPaperPlane className="chat__send-icon" />
         </button>
+        <button className="chat__more-button" onClick={onMoreButtonClick}>
+          <FaRegSquarePlus className="chat__more-icon" />
+        </button>
+      </form>
+
+      <div
+        className={`chat__connected-more-box ${
+          inputButtons ? "chat__connected-more-box-active" : ""
+        }`}
+      >
+        <ul className="chat__connected-more-list">
+          <li className="chat__connected-more-item">
+            <button className="chat__connected-emoji-button">
+              <FaFaceLaughBeam className="chat__connected-emoji-icon" />
+            </button>
+          </li>
+          <li className="chat__connected-more-item">
+            <button className="chat__connected-leave-button" onClick={onLeaveChat}>
+              Leave chat
+              <FaArrowRightToBracket className="chat__connected-leave-icon" />
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/store/slices/chatSlice.ts
+++ src/store/slices/chatSlice.ts
@@ -5,15 +5,17 @@ interface ChatState {
   connected: boolean;
   userImage: string | null;
   userName: string | null;
+  messages: { message: string; type: "left" | "right" }[];
 }
 
 const chatSlice = createSlice({
   name: "chat",
   initialState: {
     connecting: false,
-    connected: true,
+    connected: false,
     userImage: null,
     userName: null,
+    messages: [],
   },
   reducers: {
     setConnecting: (state: ChatState, action: { payload: boolean }) => {
@@ -28,11 +30,29 @@ const chatSlice = createSlice({
     setUserName: (state: ChatState, action: { payload: string }) => {
       state.userName = action.payload;
     },
+    addMessage: (
+      state: ChatState,
+      action: { payload: { message: string; type: "left" | "right" } }
+    ) => {
+      state.messages.push({
+        message: action.payload.message,
+        type: action.payload.type,
+      });
+    },
+    removeMessages: (state: ChatState) => {
+      state.messages = [];
+    }
   },
 });
 export default chatSlice.reducer;
-export const { setConnecting, setConnected, setUserImage, setUserName } =
-  chatSlice.actions;
+export const {
+  setConnecting,
+  setConnected,
+  setUserImage,
+  setUserName,
+  addMessage,
+  removeMessages,
+} = chatSlice.actions;
 export const selectConnecting = (state: { chat: ChatState }) =>
   state.chat.connecting;
 export const selectConnected = (state: { chat: ChatState }) =>
@@ -41,3 +61,5 @@ export const selectUserImage = (state: { chat: ChatState }) =>
   state.chat.userImage;
 export const selectUserName = (state: { chat: ChatState }) =>
   state.chat.userName;
+export const selectMessages = (state: { chat: ChatState }) =>
+  state.chat.messages;
